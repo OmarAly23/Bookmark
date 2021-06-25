6d268f2c2d5b8b6fe2440ca2d2c24b3c5e38a44f
Omar-aly <omar.aly@sci.ku.edu.kw>
2021-06-26 01:17:35 +0300

    Modifications in the validation form

--- src/Validation.js
+++ src/Validation.js
@@ -1,14 +1,14 @@
 function Validation(email, password, confirmPass) {
     
     function checkConfirmation(password, confirmPass) {
-        if (password !== confirmPass) {
+        if (password.value !== confirmPass.value) {
             return false;
         }
         return true;
     }
 
     function checkPass(password) {
-        var regex = /^(?=.*[a-z])(?=.*\d)(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?=.*[!@#$%]).{8, }$/;
+        var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
 
         if (password.value.match(regex)) {
             return true;
@@ -18,7 +18,7 @@ function Validation(email, password, confirmPass) {
 
     function check_Email(email) {
         // temporary regex => prone to be changed (to be checked by hussain)
-        var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        var regex = /^[a-zA-Z0-9.!#$%&'*+\=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
         if (email.value.match(regex)) {
             return true;
@@ -26,10 +26,18 @@ function Validation(email, password, confirmPass) {
         return false;
     }
 
-    if (checkConfirmation(password, confirmPass) && check_Email(email) && checkPassword(password)) {
+    if (checkConfirmation(password, confirmPass) && check_Email(email) && checkPass(password)) {
         alert('Successfully Registered');
     } else {
-        alert('Failed to Register');
+        if (!checkConfirmation(password, confirmPass)) {
+            alert('Passwords do not match');
+        }
+        if (!checkPass(password)) {
+            alert('Weak Password');
+        }
+        if (!check_Email(email)) {
+            alert('Wrong Email Format!');
+        }
     }
 
 
