32f4f406c723114a11c819329553551e16a1097e
Omar-aly <omar.aly@sci.ku.edu.kw>
2021-07-02 15:04:53 +0300

    Implementation of the backend PHP

---
+++ serverCode/db_info.php
@@ -0,0 +1,14 @@
+<?PHP
+	//DATABASE CONNECTION INFORMATION
+		$host = "localhost";
+		$user = "id16608174_bookmark_name";
+		$password = "webCS335Books@";
+		$dbname = "id16608174_bookmark";
+		$db = mysqli_connect($host, $user, $password, $dbname);
+		// Check connection
+        if (mysqli_connect_errno()) {
+            echo "Connection failed: " . mysqli_connect_error();
+            exit();
+        }
+        //echo "Connected successfully";
+?>
