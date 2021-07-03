cb87d24af859671557e5f61619306b8834104d73
Omar-aly <omar.aly@sci.ku.edu.kw>
2021-07-03 18:36:57 +0300

    Modifications in search

--- serverCode/search.php
+++ serverCode/search.php
@@ -3,7 +3,7 @@
     require "db_info.php";  //Connect to get info
     $search = $_POST['search'];
 
-    $sql = "SELECT * FROM books WHERE
+    $sql = "SELECT * FROM Books WHERE
                 ID LIKE '%$search%' OR
                 Name LIKE '%$search%' OR
                 Author LIKE '%$search%' OR
@@ -12,16 +12,17 @@
 
     if (!($result = mysqli_query($db, $sql))) {
         print("<p>Could not execute query!</p>");
-        die(mysqli_error($database) . "</body><html>");
+        die(mysqli_error($db) . "</body><html>");
     }
 
     $number = mysqli_num_rows($result);
     $pageTitle = "Search Results";
     include "header.php";
     print <<< HERE
+    <link href="../resources/style.css" rel="stylesheet" type="text/css">
     <header>
     <div class="bookmark-header">
-        <h1><img src="./images/icons8-hatena-bookmark-48-blue.png" width="55px" height="55px"><a href="homePage.html">  Bookmark</a></h1>
+        <h1><img src=".././images/icons8-hatena-bookmark-48-blue.png" width="55px" height="55px"><a href="../homePage.html">  Bookmark</a></h1>
         <ul class="nav justify-content-end">
             <li class="nav-item">
                 <input type="button" class="buttn" placeholder="Dark" value="Dark-Mode">
@@ -51,26 +52,34 @@
               <a class="nav-link active" aria-current="page" href="#about">About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="features.html">Features</a> 
+                <div class="dropdown">
+                    <p class="nav-link">Book Reviews</p>
+                    <div class="dropdown-content">
+                        <span><a class="nav-link" style="color: black;" href="reviews.php">All Reviews</a></span>
+                        <span><a class="nav-link" style="color: black;" href="fiction.php">Fiction</a></span>
+                        <span><a class="nav-link" style="color: black;" href="nonFiction.php">Non-Fiction</a></span>
+                        <span><a class="nav-link" style="color: black;" href="mystery.php">Mystery</a></span>
+                        <span><a class="nav-link" style="color: black;" href="horror.php">Horror</a></span>
+                        <span><a class="nav-link" style="color: black;" href="Comic_Books.php">Comic books</a></span>
+                    </div>
+                </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="recommendations.html">Recommendations</a>
+                <a class="nav-link" href="../login.html">Login</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="reviews.html">Reviews</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="login.html">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="register.html">Register</a>
+                <a class="nav-link" href="../register.html">Register</a>
             </li>
             <li class="nav-item search">
-                <input type="text" placeholder="Search for a book...">
+                <form method="post" action="bookmark/serverCode/search.php">
+                    <input type="text" name="search" id="search" placeholder="Search for a book...">
+                    <input type="submit" name="submit" value="Search">
+                </form>
+                
             </li>
           </ul>
         </div>
-    </header>
+</header>
 HERE;
 
 
